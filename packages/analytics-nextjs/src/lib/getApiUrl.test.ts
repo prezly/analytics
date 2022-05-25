93d1b00a68f6aeae7453c14269900e75d741cf0d
Oleg Semyonov <oleg@prezly.com>
2022-05-25 12:02:45 +0400

    Add basic test

---
+++ packages/analytics-nextjs/src/lib/getApiUrl.test.ts
@@ -0,0 +1,27 @@
+import { getApiUrl } from './getApiUrl';
+
+describe('getApiUrl', () => {
+    const { env } = process;
+
+    // This is needed to properly mock process.env, without affecting other tests.
+    // See https://www.webtips.dev/how-to-mock-processenv-in-jest
+    beforeEach(() => {
+        jest.resetModules();
+        process.env = { ...env };
+    });
+
+    afterEach(() => {
+        process.env = env;
+    });
+
+    it('returns test URL when not in production', () => {
+        expect(getApiUrl()).toBe('http://analytics.prezly.test');
+    });
+
+    it('returns production URL when in production', () => {
+        // @ts-expect-error
+        process.env.NODE_ENV = 'production';
+
+        expect(getApiUrl()).toBe('https://analytics.prezly.com');
+    });
+});
