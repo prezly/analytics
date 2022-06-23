fd1c7ee55c4123c8114d0f184ffa046f0d3bc5c4
Oleg Semyonov <oleg@prezly.com>
2022-06-23 17:06:18 +0400

    Fix incorrect export for `TrackingPolicy` enum

--- packages/analytics-nextjs/src/index.ts
+++ packages/analytics-nextjs/src/index.ts
@@ -2,4 +2,5 @@ export * from './components';
 export { AnalyticsContextProvider } from './context';
 export * from './events';
 export { useAnalytics } from './hooks';
-export type { PickedNewsroomProperties, PickedStoryProperties, TrackingPolicy } from './types';
+export type { PickedNewsroomProperties, PickedStoryProperties } from './types';
+export { TrackingPolicy } from './types';
