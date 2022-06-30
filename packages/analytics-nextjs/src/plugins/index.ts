cd0d1371d5435ab5d04e593ce3930f56448c6529
Oleg Semyonov <oleg@prezly.com>
2022-06-30 12:08:35 +0400

    Exclude test-related modules from build artifacts

--- packages/analytics-nextjs/src/plugins/index.ts
+++ packages/analytics-nextjs/src/plugins/index.ts
@@ -1,4 +1,2 @@
-export { injectPrezlyMetaPlugin } from './injectPrezlyMeta';
 export { normalizePrezlyMetaPlugin } from './normalizePrezlyMeta';
 export { sendEventToPrezlyPlugin } from './sendToPrezly';
-export { testSpyPlugin } from './testSpy';
