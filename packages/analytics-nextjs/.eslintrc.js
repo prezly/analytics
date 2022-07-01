58ee735deb62df6532f1834229279894466b71c6
Oleg Semyonov <oleg@prezly.com>
2022-07-01 17:06:02 +0400

    Fix relative path resolving in package ESLint config

--- packages/analytics-nextjs/.eslintrc.js
+++ packages/analytics-nextjs/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     parserOptions: {
         project: ['./tsconfig.eslint.json'],
+        tsconfigRootDir: __dirname,
     },
     rules: {},
     ignorePatterns: ['**/*.test.*'],
