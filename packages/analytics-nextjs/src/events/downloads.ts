d62991219916b62f945f7d4441b8a8e145e340c3
kudlajz <kudlajzzz@gmail.com>
2024-04-04 11:54:30 +0200

    Replace current events with new ones using more generic naming

---
+++ packages/analytics-nextjs/src/events/downloads.ts
@@ -0,0 +1,6 @@
+export const DOWNLOAD = {
+    ATTACHMENT: 'Attachment download',
+    GALLERY_IMAGE: 'Gallery image download',
+    IMAGE: 'Image download',
+    MEDIA_GALLERY: 'Media gallery download',
+};
