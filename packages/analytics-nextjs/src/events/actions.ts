ce062da5134561017d5449a516b602a01628a001
kudlajz <kudlajzzz@gmail.com>
2024-04-10 11:13:02 +0200

    Track all outbound clicks

--- packages/analytics-nextjs/src/events/actions.ts
+++ packages/analytics-nextjs/src/events/actions.ts
@@ -1,4 +1,5 @@
 export const ACTIONS = {
+    OUTBOUND_LINK_CLICK: 'Outbound link click',
     SEARCH: 'Search',
     SUBSCRIBE_FORM_SUBMIT: 'Subscribe form submit',
     SWITCH_LANGUAGE: 'Switch language',
