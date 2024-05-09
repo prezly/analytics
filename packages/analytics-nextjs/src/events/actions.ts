bb5bf3734e5b3fd3c88f45b2b699aa6e6cafbdfb
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-05-09 13:16:58 +0200

    [CARE-4941] Flatten the `events` module structure

--- packages/analytics-nextjs/src/events/actions.ts
+++
@@ -1,6 +0,0 @@
-export const ACTIONS = {
-    OUTBOUND_LINK_CLICK: 'Outbound link click',
-    SEARCH: 'Search',
-    SUBSCRIBE_FORM_SUBMIT: 'Subscribe form submit',
-    SWITCH_LANGUAGE: 'Switch language',
-};
--- packages/analytics-nextjs/src/events/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './actions';
-export * from './downloads';
-export * from './media';
-export * from './view';
