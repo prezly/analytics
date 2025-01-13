d4c61fbf4924183da2a9b8dccf618d6135af6875
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-13 14:57:46 +0700

    Add injectPrezlyMetaFromPropsPlugin

--- packages/analytics-nextjs/src/plugins/index.ts
+++ packages/analytics-nextjs/src/plugins/index.ts
@@ -1,3 +1,4 @@
 export { logToConsole } from './logToConsole';
 export { normalizePrezlyMetaPlugin } from './normalizePrezlyMeta';
+export { injectPrezlyMetaFromPropsPlugin } from './injectPrezlyMetaFromProps';
 export { sendEventToPrezlyPlugin } from './sendToPrezly';
