ad866c9df9e7a54004b3b9537d263c3382a3d95b
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-14 14:12:51 +0700

    Refactor injectPrezlyMeta to work based on props and not meta tags

--- packages/analytics-nextjs/src/plugins/index.ts
+++ packages/analytics-nextjs/src/plugins/index.ts
@@ -1,4 +1,3 @@
 export { logToConsole } from './logToConsole';
 export { normalizePrezlyMetaPlugin } from './normalizePrezlyMeta';
-export { injectPrezlyMetaFromPropsPlugin } from './injectPrezlyMetaFromProps';
 export { sendEventToPrezlyPlugin } from './sendToPrezly';
