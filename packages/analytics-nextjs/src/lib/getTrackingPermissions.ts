ac9090358e34b2fbe169f4c0cd4ffd97741ec9f4
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-15 19:50:57 +0700

    Refactor getTrackingPermissions

--- packages/analytics-nextjs/src/lib/getTrackingPermissions.ts
+++ packages/analytics-nextjs/src/lib/getTrackingPermissions.ts
@@ -2,36 +2,27 @@ import { ConsentCategory, TrackingPolicy } from '../types';
 import type { Consent, TrackingPermissions } from '../types';
 
 interface Arg {
+    segmentWriteKey?: string;
     consent: Consent;
     trackingPolicy: TrackingPolicy;
 }
 
-export function getTrackingPermissions({ consent, trackingPolicy }: Arg): TrackingPermissions {
-    const canIdentify = Boolean(
-        trackingPolicy === TrackingPolicy.LENIENT ||
-            consent.categories.includes(ConsentCategory.FIRST_PARTY_ANALYTICS) ||
-            consent.categories.includes(ConsentCategory.THIRD_PARTY_COOKIES),
-    );
+export function getTrackingPermissions({
+    segmentWriteKey,
+    consent,
+    trackingPolicy,
+}: Arg): TrackingPermissions {
+    const canTrackThirdParty = consent.categories.includes(ConsentCategory.THIRD_PARTY_COOKIES);
+    const canTrackFirstParty =
+        canTrackThirdParty || consent.categories.includes(ConsentCategory.FIRST_PARTY_ANALYTICS);
 
-    const canTrackToPrezly = Boolean(
-        trackingPolicy !== TrackingPolicy.STRICT ||
-            consent.categories.includes(ConsentCategory.FIRST_PARTY_ANALYTICS),
-    );
-
-    const canTrackToSegment = Boolean(
-        trackingPolicy === TrackingPolicy.LENIENT ||
-            consent.categories.includes(ConsentCategory.THIRD_PARTY_COOKIES),
-    );
-
-    const canTrackToGoogle = Boolean(
-        trackingPolicy === TrackingPolicy.LENIENT ||
-            consent.categories.includes(ConsentCategory.THIRD_PARTY_COOKIES),
-    );
-
-    const canTrackToPlausible = Boolean(
-        trackingPolicy === TrackingPolicy.LENIENT ||
-            consent.categories.includes(ConsentCategory.FIRST_PARTY_ANALYTICS),
-    );
+    const canIdentify = trackingPolicy === TrackingPolicy.LENIENT || canTrackFirstParty;
+    const canTrackToPrezly = trackingPolicy !== TrackingPolicy.STRICT || canTrackFirstParty;
+    const canTrackToGoogle = trackingPolicy === TrackingPolicy.LENIENT || canTrackThirdParty;
+    const canTrackToPlausible = trackingPolicy === TrackingPolicy.LENIENT || canTrackFirstParty;
+    const canTrackToSegment =
+        Boolean(segmentWriteKey) &&
+        (trackingPolicy === TrackingPolicy.LENIENT || canTrackThirdParty);
 
     return {
         canIdentify,
