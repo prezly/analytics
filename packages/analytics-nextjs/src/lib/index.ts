c527d9d8f2cf211b73bc290c37cf3deaef82c147
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-16 00:21:07 +0700

    Remove unused code

--- packages/analytics-nextjs/src/lib/index.ts
+++ packages/analytics-nextjs/src/lib/index.ts
@@ -2,6 +2,5 @@ export * from './getApiUrl';
 export * from './getRecipientInfo';
 export * from './getTrackingPermissions';
 export * from './isNavigatorTrackingAllowed';
-export * from './isTrackingCookieAllowed';
 export * from './stringify';
 export * from './urlParameters';
