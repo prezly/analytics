8a3cff456f8230441bbe265fc3f2d8c1576cf446
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-24 12:28:04 +0700

    Add `noop` util

--- packages/analytics-nextjs/src/lib/index.ts
+++ packages/analytics-nextjs/src/lib/index.ts
@@ -2,5 +2,6 @@ export * from './getApiUrl';
 export * from './getRecipientInfo';
 export * from './getTrackingPermissions';
 export * from './isNavigatorTrackingAllowed';
+export * from './noop';
 export * from './stringify';
 export * from './urlParameters';
