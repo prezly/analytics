f101ca8f9ba25afdffe8f13e4feacb0e61124c5e
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-28 20:52:03 +0700

    Bring back `normalizePrezlyMetaPlugin`

--- packages/analytics-nextjs/src/plugins/index.ts
+++ packages/analytics-nextjs/src/plugins/index.ts
@@ -1,4 +1,5 @@
-export { logToConsole } from './logToConsole';
 export { injectPrezlyMetaPlugin } from './injectPrezlyMeta';
+export { logToConsole } from './logToConsole';
+export { normalizePrezlyMetaPlugin } from './normalizePrezlyMetaPlugin';
 export { sendEventToPlausiblePlugin } from './sendToPlausible';
 export { sendEventToPrezlyPlugin } from './sendToPrezly';
