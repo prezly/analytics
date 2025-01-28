c0fd7415dbd1b6d8fe7dabf13b796282b8c617bb
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-29 02:42:14 +0700

    Remove `sendEventToPlausiblePlugin`

--- packages/analytics-nextjs/src/plugins/index.ts
+++ packages/analytics-nextjs/src/plugins/index.ts
@@ -1,4 +1,3 @@
 export { logToConsole } from './logToConsole';
 export { normalizePrezlyMetaPlugin } from './normalizePrezlyMetaPlugin';
-export { sendEventToPlausiblePlugin } from './sendToPlausible';
 export { sendEventToPrezlyPlugin } from './sendToPrezly';
