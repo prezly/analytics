a821ff9f6300b2f97b5801df70f7ba5c3a39411b
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-13 13:19:45 +0700

    Update analytics re-initialization check

--- packages/analytics-nextjs/src/Analytics.ts
+++ packages/analytics-nextjs/src/Analytics.ts
@@ -68,7 +68,8 @@ export class Analytics {
     }
 
     public async init(config: Config) {
-        if (this.isInitialized) {
+        if (this.config) {
+            // Cannot re-initialize analytics
             return;
         }
 
