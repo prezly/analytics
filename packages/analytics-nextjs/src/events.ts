418f5a709239e71ed531ae6ba53c2025983535b2
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-18 13:12:31 +0700

    Update events

--- packages/analytics-nextjs/src/events.ts
+++ packages/analytics-nextjs/src/events.ts
@@ -3,12 +3,12 @@ export const ACTIONS = {
     SEARCH: 'Search',
     SUBSCRIBE_FORM_SUBMIT: 'Subscribe form submit',
     SWITCH_LANGUAGE: 'Switch language',
-    COPY_STORY_LINK: 'Newsroom - Story Page - Story link copy',
-    COPY_STORY_TEXT: 'Newsroom - Story Page - Story text copy',
+    COPY_STORY_LINK: 'Newsroom - Story Page - Copy story link',
+    COPY_STORY_TEXT: 'Newsroom - Story Page - Copy story text',
     SHARE_STORY_TO_SOCIAL_NETWORK(context: 'Gallery Page' | 'Story Page') {
         return `Newsroom - ${context} - Share story to social network`;
     },
-    RELATED_STORY_OPEN: 'Newsroom - Story Page - Related story open',
+    RELATED_STORY_OPEN: 'Newsroom - Story Page - Open related story',
 };
 
 export const DOWNLOAD = {
@@ -16,8 +16,8 @@ export const DOWNLOAD = {
     GALLERY_IMAGE: 'Gallery image download',
     IMAGE: 'Image download',
     MEDIA_GALLERY: 'Media gallery download',
-    DOWNLOAD_STORY_PDF: 'Newsroom - Story Page - Story PDF download',
-    DOWNLAOD_STORY_ASSETS: 'Newsroom - Story Page - Story assets download',
+    DOWNLOAD_STORY_PDF: 'Newsroom - Story Page - Download story PDF',
+    DOWNLAOD_STORY_ASSETS: 'Newsroom - Story Page - Download story assets',
 };
 
 export const MEDIA = {
